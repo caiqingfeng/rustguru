b19d81f07c05728197e2a33ffcbf74e45dd1df6f
蔡庆丰 <caiqingfeng@roadclouding.com>
2020-03-09 20:09:18 +0800

    readme

--- advanced-rust/macro_sample/src/main.rs
+++ advanced-rust/macro_sample/src/main.rs
@@ -1,9 +1,20 @@
 use hello_macro::HelloMacro;
 use hello_macro_derive::HelloMacro;
+use sp_debug_derive::RuntimeDebug;
 
 #[derive(HelloMacro)]
-struct Pancakes;
+#[derive(RuntimeDebug)]
+struct Pancakes(u64, u64);
 
 fn main() {
     Pancakes::hello_macro();
+    println!("{:?}", Pancakes(0, 0));
+}
+
+#[test]
+fn should_display_proper_debug() {
+	assert_eq!(
+		format!("{:?}", Pancakes(1, 1)),
+		"Pancakes(1, 1)"
+	);
 }
